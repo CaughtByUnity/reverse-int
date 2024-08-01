3a1c3fba3fcaa1b2cdc61ca4d196a616b008ba48
CaughtByUnity <xammer3500@gmail.com>
2024-08-01 11:04:56 +0300

    init: start task reverse int (and finish with ready function)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const convertToString = String(n)
+    let convertToArray = convertToString.split('')
+    let arrayLength = convertToArray.length
+    let arrayIndex = 0
+    let reverseArray = []
+    while (arrayIndex < arrayLength) {
+        reverseArray.unshift(convertToArray[arrayIndex])
+        arrayIndex++
+    }
+    reverseString = reverseArray.join('')
+    reverseNumber = parseInt(reverseString)
+    return reverseNumber
 }
